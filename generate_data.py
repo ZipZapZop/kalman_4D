d68b89dd2b8902cb2982f0884aa7f555fa0a0f0f
Niklas Hehenkamp <niklas.hehenkamp@thi.de>
2018-07-19 11:10:16 +0200

    numpy array errors

--- generate_data.py
+++ generate_data.py
@@ -1,25 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import itertools
 
 ''' Computes x, y, v_x, v_y assuming no acceleration and constant initial velocities of 1.
     Returns as a numpy array '''
     
-def generate_true_values(num_estimates, dt, x_init, y_init, x_y_only = 'False'):
+def generate_true_values(num_estimates, dt, x_init, y_init, x_y_only = False):
     x = np.zeros((4, num_estimates))
 
     # init state vector
-    x[:,[0]] = np.array([x_init, y_init, 1, 1]).T
-    A = np.array( [1, 0, dt, 0],
+    init = np.array([x_init, y_init, 1, 1])
+    init = init.reshape((-1,1))
+    x[:, [0]] = init
+    A = np.array([[1, 0, dt, 0],
                   [0, 1, 0, dt],
                   [0, 0, 1, 0 ],
-                  [0, 0, 0, 1 ])
+                  [0, 0, 0, 1 ]])
 
     time_int = dt*np.linspace(1, num_estimates, num_estimates)
     it = np.nditer(time_int, flags = ['c_index'])
     it.iternext() # continue past init values
     while not it.finished:
-        x[:,[it.index]] = np.dot(A,x[:,[it.index - 1]])
+        x[:, [it.index]] = np.dot(A,x[:, [it.index - 1]])
         it.iternext()
     
     it.reset()
@@ -34,3 +35,8 @@ def generate_true_values(num_estimates, dt, x_init, y_init, x_y_only = 'False'):
         it.iternext()
 
     return pos_only
+
+
+
+x = generate_true_values(10,0.1,5,5)
+print(x)
