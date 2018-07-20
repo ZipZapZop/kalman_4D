333cc1e2dc58948938a4ae5d051fef4363c10862
Biraj Parikh <bparikh@u.northwestern.edu>
2018-07-20 14:42:02 +0200

    Implemented gain and innovation; Created plotting function

--- kalman.py
+++ kalman.py
@@ -6,7 +6,7 @@ def kalman_filter(num_trials, x_init, y_init):
     dt = 0.001
     # std_dev_x and std_dev_y of sensors is 3m
     # generate_noisy_values(num_trials, dt, std_dev_x, std_dev_y, x_init, y_init)
-    noisy_readings = generate_data.generate_noisy_values(num_trials, dt, 2, 2, x_init,y_init)
+    noisy_readings = generate_data.generate_noisy_values(num_trials, dt, 2, 2, x_init,y_init)   # 4xnum_trials
 
     A = np.array([[1, 0, dt, 0],
                 [0, 1, 0, dt],
@@ -17,7 +17,7 @@ def kalman_filter(num_trials, x_init, y_init):
 
     # Init var of x and y are large because uncertain of original position.
     # Init var of vel_x and vel_y are 0.1.
-    # There are no covariances as each state var is independent.
+    # All covariances are equal to 0 as each state var is assumed independent.
 
     P = np.array([ [500, 0, 0, 0],
                     [0, 500, 0, 0],
@@ -42,7 +42,36 @@ def kalman_filter(num_trials, x_init, y_init):
         # gain
         K_num = np.dot(P, H.T)
         K_denom = np.dot(np.dot(H,P),H.T) + R
+        K = np.dot(K_num,np.linalg.inv(K_denom))
 
-    # TODO: K is current a 4x4 matrix, which will pose problems in the update step as the sensor simulation comes in as a 2x1 instead of a 4x1
-    # To remedy, change the sensor measurements to be 4x1 (include v_x and v_y). This can be done with x_y_only.
-    # Plot everything after this
+        innovation = noisy_readings[:,[i]] - np.dot(H,state[:,[i-1]])
+        state[:,[i]] = state[:,[i]]+ np.vstack(np.dot(K,innovation))
+        P = np.dot(np.eye(4) - np.dot(K,H),P)
+
+    return state
+
+x = kalman_filter(10,2,2)
+print(np.shape(x))
+print(x)
+
+def plot_kalman(num_trials, x_init, y_init):
+    fig = plt.figure()
+    states = kalman_filter(num_trials, x_init, y_init) # 4 x num_trials
+    time_ints = np.linspace(1, num_trials,num_trials).tolist()
+    estimates = generate_data.generate_true_values(num_trials,0.001,x_init,y_init,x_y_only=False)
+
+    ax1 = plt.subplot(211)  # x values
+    plt.plot(states[0],label = 'Predicted values')
+    plt.plot(estimates[0],label = 'Estimated values')
+    handles, labels = ax1.get_legend_handles_labels()
+
+    ax2 = plt.subplot(212)  # y values
+    plt.plot(states[1])
+    plt.plot(estimates[1])
+    # handles and labels for ax2 are same as those for ax1
+    
+    fig.legend(handles, labels)
+
+    plt.show()
+
+plot_kalman(1000,2,2)
