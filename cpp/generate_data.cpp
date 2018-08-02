53056b4c7d2794a44ad95ad5aff9e4106c6148ba
Biraj Parikh <bparikh@u.northwestern.edu>
2018-08-02 12:35:35 +0200

    Changed zeros matrix to uninit matrix

--- cpp/generate_data.cpp
+++ cpp/generate_data.cpp
@@ -8,13 +8,12 @@ using namespace Eigen;
 MatrixXf generate_true_values(int num_trials, double dt, double x_init, double y_init, double a_x, double a_y, bool x_y_only) {
     std::cout << "generate_true_vals started\n";
     // matrix of floats of size 2xnum_trials (col # is dynamic)
-    MatrixXf x = MatrixXf::Zero(4,num_trials);
+    MatrixXf x(4,num_trials);
     // Vector2f u(a_x, a_y);
 
     Vector4f init(x_init, y_init, 0, 0);
     x.col(0) = init;
 
-    std::cout << x << std::endl;
     std::cout << "generate_true_vals ended\n";
     return x;
 }
@@ -27,7 +26,7 @@ int main() {
     double a_x = 0.1;
     double a_y = 0.1;
     bool x_y_only = false;
-    
+
     MatrixXf r = generate_true_values(num_trials, dt, x_init, y_init, a_x, a_y, x_y_only);
     std::cout << r << std::endl;
     return 0;
