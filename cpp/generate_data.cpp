814018aafbf4cc120e8643713b44a90b172f40a1
Biraj Parikh <bparikh@u.northwestern.edu>
2018-08-03 17:26:31 +0200

    csv export function

--- cpp/generate_data.cpp
+++ cpp/generate_data.cpp
@@ -2,19 +2,19 @@
 
 using namespace Eigen;
 
-MatrixXd generate_true_values(int num_trials, double dt, double x_init, double y_init, double a_x, double a_y, bool x_y_only) {
+MatrixXd generate_true_values(int num_trials, double dt, double x_init, double y_init, double a_x, double a_y) {
     // matrix of floats of size 2xn
     MatrixXd x(4,num_trials);
     Vector2d u(a_x, a_y);
 
     Vector4d init(x_init, y_init, 0, 0);
     x.col(0) = init;
-    double dt_sq = dt*dt;
+   double dt_sq = dt*dt;
 
     Matrix4d A;
     A << 1, 0, dt, 0,
          0, 1, 0, dt, 
-         0, 0, 0, 1,
+         0, 0, 1, 0,
          0, 0, 0, 1;
     
     MatrixXd B(4,2);
@@ -37,7 +37,7 @@ MatrixXd generate_noisy_values(int num_trials, double dt, double std_dev_x, doub
     std::normal_distribution<> Nx(0, std_dev_x);
     std::normal_distribution<> Ny(0, std_dev_y);
 
-    MatrixXd true_vals = generate_true_values(num_trials, dt, x_init, y_init, a_x, a_y, false);
+    MatrixXd true_vals = generate_true_values(num_trials, dt, x_init, y_init, a_x, a_y);
 
     MatrixXd sensor_values(4,num_trials);
 
@@ -51,17 +51,7 @@ MatrixXd generate_noisy_values(int num_trials, double dt, double std_dev_x, doub
     return sensor_values;
 } 
 
-int main() {
-    int num_trials = 1000;
-    double dt = 0.001;
-    double x_init = 2;
-    double y_init = 2;
-    double std_dev_x = 3;
-    double std_dev_y = 3;
-    double a_x = 0.1;
-    double a_y = 0.1;
-    bool x_y_only = false;
-
+void export_noisy_to_csv(int num_trials, double dt, double std_dev_x, double std_dev_y, double x_init, double y_init, double a_x, double a_y) {
     MatrixXd r = generate_noisy_values(num_trials, dt, std_dev_x, std_dev_y, x_init, y_init, a_x, a_y);
 
     std::ofstream data_out;
@@ -74,7 +64,18 @@ int main() {
     for(int i = 0; i < num_trials; ++i)
         data_out << r(1,i) << '\n';
     data_out.close();
-    return 0;
+}
+
+int main() {
+    int num_trials = 1000;
+    double dt = 0.001;
+    double x_init = 2;
+    double y_init = 2;
+    double std_dev_x = 3;
+    double std_dev_y = 3;
+    double a_x = 0.1;
+    double a_y = 0.1;
+    export_noisy_to_csv(num_trials, dt, std_dev_x, std_dev_y, x_init, y_init, a_x, a_y);
 }
 
 
