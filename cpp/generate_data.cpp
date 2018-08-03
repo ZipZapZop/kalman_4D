4a55485c4df48d09a32b9794a36a361638651421
Biraj Parikh <bparikh@u.northwestern.edu>
2018-08-03 14:26:31 +0200

    Noisy value generator

--- cpp/generate_data.cpp
+++ cpp/generate_data.cpp
@@ -1,18 +1,14 @@
-#include <iostream>
-#include <random>
-#include <cmath>
-#include <eigen3/Eigen/Dense>
+#include "generate_data.hpp"
 
 using namespace Eigen;
 
 MatrixXd generate_true_values(int num_trials, double dt, double x_init, double y_init, double a_x, double a_y, bool x_y_only) {
-    // matrix of floats of size 2xnum_trials (col # is dynamic)
+    // matrix of floats of size 2xn
     MatrixXd x(4,num_trials);
     Vector2d u(a_x, a_y);
 
     Vector4d init(x_init, y_init, 0, 0);
     x.col(0) = init;
-    std::cout << x << std::endl;
     double dt_sq = dt*dt;
 
     Matrix4d A;
@@ -30,21 +26,55 @@ MatrixXd generate_true_values(int num_trials, double dt, double x_init, double y
     for(int i = 1; i < num_trials; ++i)
         x.col(i) = A*x.col(i - 1) + B*u;
 
-    
-    // std::cout << time_int << std::endl;
     return x;
 }
 
+MatrixXd generate_noisy_values(int num_trials, double dt, double std_dev_x, double std_dev_y, double x_init, double y_init, double a_x, double a_y) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::default_random_engine generator;
+    std::normal_distribution<> Nx(0, std_dev_x);
+    std::normal_distribution<> Ny(0, std_dev_y);
+
+    MatrixXd true_vals = generate_true_values(num_trials, dt, x_init, y_init, a_x, a_y, false);
+
+    MatrixXd sensor_values(4,num_trials);
+
+    for(int i = 0; i < num_trials; ++i) {
+        sensor_values(0,i) = true_vals(0, i) + Nx(gen);
+        sensor_values(1,i) = true_vals(1, i) + Ny(gen);
+        sensor_values(2,i) = true_vals(2, i) + Nx(gen);  
+        sensor_values(3,i) = true_vals(3, i) + Ny(gen);
+    }
+
+    return sensor_values;
+} 
+
 int main() {
-    int num_trials = 10;
+    int num_trials = 1000;
     double dt = 0.001;
-    double x_init = 1;
-    double y_init = 1;
+    double x_init = 2;
+    double y_init = 2;
+    double std_dev_x = 3;
+    double std_dev_y = 3;
     double a_x = 0.1;
     double a_y = 0.1;
     bool x_y_only = false;
 
-    MatrixXd r = generate_true_values(num_trials, dt, x_init, y_init, a_x, a_y, x_y_only);
-    std::cout << r << std::endl;
+    MatrixXd r = generate_noisy_values(num_trials, dt, std_dev_x, std_dev_y, x_init, y_init, a_x, a_y);
+
+    std::ofstream data_out;
+    data_out.open("noisy_data_x.csv");
+    for(int i = 0; i < num_trials; ++i)
+        data_out << r(0,i) << '\n';
+    data_out.close();
+
+    data_out.open("noisy_data_y.csv");
+    for(int i = 0; i < num_trials; ++i)
+        data_out << r(1,i) << '\n';
+    data_out.close();
     return 0;
 }
+
+
