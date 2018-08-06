ecb477c40b919d44f381635202e57c1b92257694
Biraj Parikh <bparikh@u.northwestern.edu>
2018-08-06 18:15:17 +0200

    Added csv writer

--- cpp/test_eigen/test.cpp
+++ cpp/test_eigen/test.cpp
@@ -1,17 +1,23 @@
-
 #include <iostream>
+#include <fstream>
 #include <random>
 #include <cmath>
 #include <eigen3/Eigen/Dense>
+#include "generate_data.hpp"
 
 using namespace Eigen;
 
-int main() {
-    MatrixXd x(4,8);
+const int num_trials = 1000;
+
+MatrixXd kalman_filter() {
+    MatrixXd x(4,num_trials);
     Vector4d init(1, 1, 0, 0);
     x.col(0) = init;
     double dt = 0.001;
     Vector2d u(2, 2);
+
+    Matrix4Xd noisy_readings = generate_noisy_values(num_trials, dt, 3, 3, 1, 1, 2, 2);
+
 
     double dt_sq = dt*dt;
 
@@ -33,21 +39,56 @@ int main() {
          0, 0, 0.1, 0,
          0, 0, 0, 0.1;
 
+    Matrix4d R;
+    R << 9, 0, 0, 0,
+         0, 9, 0, 0,
+         0, 0, 0.01, 0,
+         0, 0, 0, 0.01;
+
     Matrix4d H = Matrix4d::Identity();
-    Matrix4Xd state(4, 8);
+    Matrix4Xd state(4, num_trials);
     state.col(0) = Vector4d(0, 0, 1, 1); 
     Matrix4d Q = Matrix4d::Zero(); 
-
-    for(int i = 1; i < 8; ++i) { 
+    Matrix4d K;
+    Vector4d innovation;
+    for(int i = 1; i < num_trials; ++i) { 
         state.col(i) = A*state.col(i - 1) + B*u;
-        P = (A*P)*A.transpose();
-        P = P + Q;
+        P = (A*P)*A.transpose() + Q;
 
         Matrix4d K_num = P*(H.transpose());
         Matrix4d K_denom = (H*P);
+        K_denom = H*K_denom*H.transpose();
+        K_denom = K_denom + R;
+        K = K_num*(K_denom.inverse());
 
-    //    x.col(i) = A*x.col(i - 1) + B*;
+        Vector4d innovation = noisy_readings.col(i) - H*state.col(i);
+        // std::cout << innovation << std::endl;
+        state.col(i) += K*innovation;
+        P = (Matrix4d::Identity() - (K*H))*P;
     }
-    std::cout << state << std::endl;
-    return 0;
+    // std::cout << state << std::endl;
+    return state;
+}
+
+void filtered_to_csv() {
+    MatrixXd filtered = kalman_filter();
+    MatrixXd ideal = generate_true_values(num_trials, 0.001, 1, 1, 2, 2);
+
+    std::ofstream data_out;
+    data_out.open("test_x.csv");
+
+    for(int i = 0; i < num_trials; ++i)
+        data_out << filtered(0,i) << '\n';
+    data_out.close();
+
+    data_out.open("test_y.csv");
+    for(int i = 0; i < num_trials; ++i)
+        data_out << filtered(1,i) << '\n';
+    data_out.close();
+
+    
+}
+
+int main() {
+    filtered_to_csv();
 }
