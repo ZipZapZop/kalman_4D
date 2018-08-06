ab33e69a899601f03cb33c4c7aa6c0802509afa7
Biraj Parikh <bparikh@u.northwestern.edu>
2018-08-06 15:55:03 +0200

    int matrices to double matrices; filtered_to_csv(); main()

--- cpp/generate_data.cpp
+++ cpp/generate_data.cpp
@@ -66,16 +66,5 @@ void export_noisy_to_csv(int num_trials, double dt, double std_dev_x, double std
     data_out.close();
 }
 
-int main() {
-    int num_trials = 1000;
-    double dt = 0.001;
-    double x_init = 2;
-    double y_init = 2;
-    double std_dev_x = 3;
-    double std_dev_y = 3;
-    double a_x = 0.1;
-    double a_y = 0.1;
-    export_noisy_to_csv(num_trials, dt, std_dev_x, std_dev_y, x_init, y_init, a_x, a_y);
-}
 
 
