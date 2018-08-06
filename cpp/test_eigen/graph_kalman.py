0a71555d5b834a48fbcc45a49a7f04784b15a35f
Biraj Parikh <bparikh@u.northwestern.edu>
2018-08-06 18:35:58 +0200

    ideal x and y plotting

--- cpp/test_eigen/graph_kalman.py
+++ cpp/test_eigen/graph_kalman.py
@@ -5,9 +5,22 @@ def print_graph():
     with open('test_x.csv','r') as d:
         lines = [line.rstrip(' ') for line in open('test_x.csv')]
         lines = list(map(float, lines))
-            
+    d.close()
+
+    with open('true_x.csv','r') as e:
+        reals = [real.rstrip(' ') for real in open('true_x.csv')]
+        reals = list(map(float, reals))
+    e.close()
+
+    # add y plotting
+    with open('true_y.csv','r') as f:
+        reals = [real.rstrip(' ') for real in open('true_x.csv')]
+        reals = list(map(float, reals))
+    e.close()
+
     plt.figure()
-    plt.plot(lines,'k-',label='Kalman filter outputs')
+    plt.plot(lines,label='Kalman filter outputs')
+    plt.plot(reals)
     plt.legend()
     plt.title('')
     plt.show()
