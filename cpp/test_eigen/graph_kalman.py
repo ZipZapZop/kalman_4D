4db7c87f0cdf9d10388755a23237cafe2373f43b
Biraj Parikh <bparikh@u.northwestern.edu>
2018-08-06 18:14:57 +0200

    init

---
+++ cpp/test_eigen/graph_kalman.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import csv
+
+def print_graph():
+    with open('test_x.csv','r') as d:
+        lines = [line.rstrip(' ') for line in open('test_x.csv')]
+        lines = list(map(float, lines))
+            
+    plt.figure()
+    plt.plot(lines,'k-',label='Kalman filter outputs')
+    plt.legend()
+    plt.title('')
+    plt.show()
+
+print_graph()
