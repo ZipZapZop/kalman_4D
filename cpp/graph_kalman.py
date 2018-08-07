d394c815fbf4c08c18812032031bfcf8318599c6
Biraj Parikh <bparikh@u.northwestern.edu>
2018-08-07 16:08:03 +0200

    Copied over testing code

--- cpp/graph_kalman.py
+++ cpp/graph_kalman.py
@@ -2,14 +2,36 @@ import matplotlib.pyplot as plt
 import csv
 
 def print_graph():
-    with open('filtered_data_x.csv','r'):
-        lines = [line.rstrip(' ') for line in open('filtered_data_x.csv')]
-        lines = list(map(float, lines))
+    with open('test_x.csv','r') as d:
+        filtered_x = [line.rstrip(' ') for line in open('test_x.csv')]
+        filtered_x = list(map(float, filtered_x))
+    d.close()
+
+    with open('test_y.csv','r') as e:
+        filtered_y = [line.rstrip(' ') for line in open('test_y.csv')]
+        filtered_y = list(map(float, filtered_y))
+    e.close()
+
+    # add y plotting
+    with open('ideal_x.csv','r') as f:
+        ideal_x = [line.rstrip(' ') for line in open('ideal_x.csv')]
+        ideal_x = list(map(float, ideal_x))
+    f.close()
+
+    with open('ideal_y.csv','r') as h:
+        ideal_y = [line.rstrip(' ') for line in open('ideal_y.csv')]
+        ideal_y = list(map(float, ideal_y))
+    h.close()
     
-    plt.figure()
-    plt.plot(lines,'k-',label='Kalman filter outputs')
-    plt.legend()
-    plt.title('')
+    fig = plt.figure(num=1,figsize=(10, 10), dpi=100)
+    
+    plt.plot(filtered_x, label='Estimated values in x')
+    plt.plot(filtered_y, label='Estimated values in y')
+    plt.plot(ideal_x, label='Predicted values in x')
+    plt.plot(ideal_y, label='Predicted values in y')
+
+    fig.legend()
+    
     plt.show()
 
 print_graph()
