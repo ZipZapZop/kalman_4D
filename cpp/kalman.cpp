a86dfa26f15e36191760a025440d5db81dff466f
Biraj Parikh <bparikh@u.northwestern.edu>
2018-08-07 11:47:17 +0200

    Changed to look more like test;  edited inputs

--- cpp/kalman.cpp
+++ cpp/kalman.cpp
@@ -8,7 +8,7 @@ MatrixXd kalman_filter(int num_trials, double x_init, double y_init, double a_x,
     double dt = 0.001;
     double dt_sq = dt*dt;
 
-    // set std_dev_x and std_dev_y of sensors to 3m
+    // assuming std_dev_x and std_dev_y of sensors is 3m
     Matrix4Xd noisy_readings = generate_noisy_values(num_trials, dt, 3, 3, x_init, y_init, a_x, a_y);
 
     Matrix4d A;
@@ -18,28 +18,36 @@ MatrixXd kalman_filter(int num_trials, double x_init, double y_init, double a_x,
          0, 0, 0, 1;
     
     MatrixXd B(4,2);
-    B << 0.5*dt_sq, 0,
-         0, 0.5*dt_sq,
-         dt, 0,
-         0, dt;
-    
-    Matrix4d H = Matrix4d::Identity();
+    B << 0.5*dt_sq,     0,
+         0,             0.5*dt_sq,
+         dt,            0,
+         0,             dt;
 
     // Init var of x and y are large because uncertain of original position.
     // Vel_x and vel_y are 0.1.
+    
     // All covariances are equal to 0 as each state var is assumed independent.
-
+    // P diagonal is the variances each state var
     Matrix4d P;
     P << 500, 0, 0, 0,
          0, 500, 0, 0,
          0, 0, 0.1, 0,
          0, 0, 0, 0.1;
-    
+
+    //assuming a lack of process noise
+    Vector4d W = Vector4d::Zero();
+    // process noise covariance is set to 0 as process noise is 0
+    Matrix4d Q = Matrix4d::Zero();
+    // This is just to convert between matrix dimensions
+    Matrix4d H = Matrix4d::Identity();
+
+    // to store the variances
     Matrix4Xd variances(4, num_trials);
 
-
-    Matrix4d Q = Matrix4d::Zero();  ////assuming a lack of process noise
-
+    // sensor noise covariance matrix
+    // std_dev_x and std_dev_y is 3m; std_dev_vx and std_dev_vy are sqrt(0.01)
+    // this is definitely too low of a variance
+    // TODO: Play around with this variance to see effect
     Matrix4d R;
     R << 9, 0, 0, 0,
          0, 9, 0, 0,
@@ -47,9 +55,8 @@ MatrixXd kalman_filter(int num_trials, double x_init, double y_init, double a_x,
          0, 0, 0, 0.01;
 
     Matrix4Xd state(4, num_trials);
-
     state.col(0) = Vector4d(0, 0, x_init, y_init); // init state
-    Vector4d W = Vector4d::Zero(); // process noise is set to 0
+
     Vector2d u(a_x, a_y);
     MatrixXd Bu = B*u;
     for(int i = 1; i < num_trials; ++i) {
@@ -95,12 +102,12 @@ void filtered_to_csv(int num_trials, double x_init, double y_init, double a_x, d
 int main() {
     int num_trials = 1000;
     double dt = 0.001;
-    double x_init = 2;
-    double y_init = 2;
+    double x_init = 1;
+    double y_init = 1;
     double std_dev_x = 3;
     double std_dev_y = 3;
-    double a_x = 0.1;
-    double a_y = 0.1;
+    double a_x = 2;
+    double a_y = 2;
 
-    filtered_to_csv(num_trials,  x_init, y_init, a_x, a_y);
+    filtered_to_csv(num_trials, x_init, y_init, a_x, a_y);
 }
