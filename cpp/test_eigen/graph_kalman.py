f581e37ce2b3abda7fe09bf49e13641a0d7e3b88
Biraj Parikh <bparikh@u.northwestern.edu>
2018-08-07 11:49:57 +0200

    Subplots

--- cpp/test_eigen/graph_kalman.py
+++ cpp/test_eigen/graph_kalman.py
@@ -3,26 +3,42 @@ import csv
 
 def print_graph():
     with open('test_x.csv','r') as d:
-        lines = [line.rstrip(' ') for line in open('test_x.csv')]
-        lines = list(map(float, lines))
+        filtered_x = [line.rstrip(' ') for line in open('test_x.csv')]
+        filtered_x = list(map(float, filtered_x))
     d.close()
 
-    with open('true_x.csv','r') as e:
-        reals = [real.rstrip(' ') for real in open('true_x.csv')]
-        reals = list(map(float, reals))
+    with open('test_y.csv','r') as e:
+        filtered_y = [line.rstrip(' ') for line in open('true_x.csv')]
+        filtered_y = list(map(float, filtered_y))
     e.close()
 
     # add y plotting
-    with open('true_y.csv','r') as f:
-        reals = [real.rstrip(' ') for real in open('true_x.csv')]
-        reals = list(map(float, reals))
-    e.close()
+    with open('true_x.csv','r') as f:
+        ideal_x = [line.rstrip(' ') for line in open('true_x.csv')]
+        ideal_x = list(map(float, ideal_x))
+    f.close()
 
-    plt.figure()
-    plt.plot(lines,label='Kalman filter outputs')
-    plt.plot(reals)
-    plt.legend()
-    plt.title('')
+    with open('true_y.csv','r') as h:
+        ideal_y = [line.rstrip(' ') for line in open('true_y.csv')]
+        ideal_y = list(map(float, ideal_y))
+    h.close()
+    
+    fig = plt.figure(num=1,figsize=(12, 10), dpi=100)
+    
+    ax1 = fig.add_subplot(112)
+    plt.plot(filtered_x, label='Estimated values')
+    plt.plot(ideal_x, label='Predicted values')
+    handles, labels = ax1.get_legend_handles_labels()
+    ax1.set_title('x position')
+
+    ax2 = fig.add_subplot(212)  # y values
+    plt.plot(filtered_y)
+    plt.plot(ideal_y)
+    ax2.set_title('y position')
+
+    fig.legend(handles, labels)     # handles and labels for ax2, ax3, ax4 are same as for ax1
+    fig.subplots_adjust(hspace=.2,wspace=.2)
+    
     plt.show()
 
 print_graph()
